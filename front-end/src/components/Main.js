322bfbb226b4994a131feeb8021f491cb8210d00
ChrGyoshev <krischan@abv.bg>
2023-10-05 11:32:20 +0300

    added navigation

---
+++ front-end/src/components/Main.js
@@ -0,0 +1,11 @@
+const Main = () => {
+  return (
+    <div className="image-container">
+      <h1 className="site-slogan">
+        From our site to your book nook, stories that will hook
+      </h1>
+    </div>
+  );
+};
+
+export default Main;
---
+++ front-end/src/components/Navbar.js
@@ -0,0 +1,32 @@
+const NavBar = () => {
+  return (
+    <>
+      <nav>
+        <div className="allMenu ">
+          <ul className="ul">
+            <li className="lia">
+              <a className="li">Home</a>{" "}
+            </li>
+
+            <li className="lia">
+              <a className="li">Catalogue</a>
+            </li>
+
+            <li className="lia">
+              <a className="li">Sign Up</a>
+            </li>
+
+            <li className="lia">
+              <a className="li">Sign In</a>
+            </li>
+
+            <li className="lia">
+              <a className="li">About</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+};
+export default NavBar;
