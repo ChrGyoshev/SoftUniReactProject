db7284e6077ad7ad3bc0d0b2ff0add8e7cdd2f80
ChrGyoshev <krischan@abv.bg>
2023-10-05 11:31:14 +0300

    navigation added

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,12 @@
+import NavBar from "./components/Navbar";
+import Main from "./components/Main";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <>
+      <NavBar />
+      <Main />
+    </>
   );
 }
 
