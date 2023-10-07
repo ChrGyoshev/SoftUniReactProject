8030cba3cb6c3d2e6b5b91aebea26a3e367a0c59
ChrGyoshev <krischan@abv.bg>
2023-10-07 18:27:33 +0300

    created .env file

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -1,11 +1,13 @@
 import logo from "../assets/pictures/logo.png";
 
 const Main = () => {
+  const apiKey = process.env.REACT_APP_API_KEY;
   return (
     <div className="image-container">
       <h1 className="site-slogan">
         From our site to your book nook, stories that will hook
       </h1>
+      
       <div className="image1">
         <img src={logo} alt="Your Picture" />
       </div>
