5b69008417062163303f330860e186857994fd07
ChrGyoshev <krischan@abv.bg>
2023-10-25 11:15:43 +0300

    removed cra

--- front-end/src/assets/js/PasswordHide.js
+++
@@ -1,7 +0,0 @@
-export default function handleClick(...clickedRef) {
-  const [input, ico] = clickedRef;
-
-  input.current.type = input.current.type === "password" ? "text" : "password";
-  ico.current.classList.toggle("fa-eye");
-  ico.current.classList.toggle("fa-eye-slash");
-}
