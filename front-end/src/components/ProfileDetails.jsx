242786fb3d4ff3837b4c8d71df3e810c03c1422f
ChrGyoshev <krischan@abv.bg>
2023-10-28 22:33:13 +0300

    added profile-details base
    added feature for upload files

--- front-end/src/components/ProfileDetails.jsx
+++ front-end/src/components/ProfileDetails.jsx
@@ -1,4 +1,33 @@
+import { auth } from "../firebase";
+import { useEffect, useState, useRef } from "react";
 const ProfileDetails = () => {
+  const [user, setUser] = useState("");
+  const imageInput = useRef(null);
+  useEffect(() => {
+    setUser(auth.currentUser);
+    if (user !== null) {
+      console.log(user);
+    }
+  }, [user]);
+
+  // const uploadImage = async () => {
+  //   let image = imageInput.current.files[0];
+  //   let userId = user.uid;
+  //   const url = "http://127.0.0.1:8000/api";
+  //   let formData = new FormData();
+  //   formData.append("profile_picture", image);
+  //   formData.append("uid", userId);
+
+  //   let newImage = await fetch(url, {
+  //     method: "POST",
+  //     body: formData,
+  //   })
+  //     .then((response) => response.json())
+  //     .catch((error) => {
+  //       console.error(error);
+  //     });
+  // };
+
   return (
     <>
       <div className="profile-card">
@@ -8,7 +37,15 @@ const ProfileDetails = () => {
         <span className="edit-profile">
           <i className="fa-solid fa-user-pen"></i>
         </span>
+        <div className="profile-image">
+          <img src={user.photoURL} alt="" />
+        </div>
+        <div className="text-data">
+          <p className="email">Email: {user.email}</p>
+        </div>
       </div>
+      {/* <input type="file" accept="image/" ref={imageInput} />
+      <button onClick={uploadImage}>upload</button> */}
     </>
   );
 };
