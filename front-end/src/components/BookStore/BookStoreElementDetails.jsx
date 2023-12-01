69f861290e918979d5c0bbfe6fb9b01a8540fb70
ChrGyoshev <krischan@abv.bg>
2023-12-01 12:37:12 +0200

    added styling for book details

--- front-end/src/components/BookStore/BookStoreElementDetails.jsx
+++ front-end/src/components/BookStore/BookStoreElementDetails.jsx
@@ -1,10 +1,10 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import styles from "./BookStoreElementDetails.module.css";
 import { useEffect, useState } from "react";
 
 const ElementDetails = () => {
   const { id } = useParams();
-  const [bookData, setBookData] = useState();
+  const [bookData, setBookData] = useState("");
   const BASE_URL = `http://localhost:8000/api/book-store/${id}/`;
 
   useEffect(() => {
@@ -19,7 +19,41 @@ const ElementDetails = () => {
       .then((result) => setBookData(result))
       .catch((error) => console.log(error));
   }, []);
-  return <></>;
+  return (
+    <>
+      <section className={styles["book-details-wrapper"]}>
+        <article className={styles["book-details"]}>
+          <div className={styles["book-details-image"]}>
+            <img src={bookData.cover} alt="" />
+          </div>
+          <article className={styles["book-details-text"]}>
+            <h1 className={styles["book-details-title"]}>{bookData.title}</h1>
+            <h2 className={styles["book-details-author"]}>{bookData.author}</h2>
+            <div className={styles["book-details-description"]}>
+              <p className={styles.description}>
+                {bookData.description
+                  ? bookData.description
+                  : "No description available"}
+              </p>
+            </div>
+            <article className={styles["book-details-price"]}>
+              <h1 className={styles["book-details-price-tag"]}>
+                {`Price: ${bookData.price} лв`}
+              </h1>
+              <article className={styles["book-details-button-wrapper"]}>
+                <div>
+                  <button>Buy now</button>
+                </div>
+                <div>
+                  <Link>Cancel</Link>
+                </div>
+              </article>
+            </article>
+          </article>
+        </article>
+      </section>
+    </>
+  );
 };
 
 export default ElementDetails;
